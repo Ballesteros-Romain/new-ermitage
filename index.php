c269e55bdf52e076270357b125167bb58b80aad5
Romain Ballesteros <romain.ballesteros@gmail.com>
2023-06-08 15:35:41 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,181 @@
+<?php require './php/header.php'; ?>
+<main>
+    <div><img class="photo-piscine rounded center" src="../PHOTOS/piscine.jpeg" alt="photo piscine"></div>
+    <div>
+        <h1>Le Clos de l'ermitage <br>Bienvenue chez Estele et Andre</h1>
+        <p class="text-center mt-3">Propriété en campagne, à 10 mn du centre de Nîmes, Jardin, Animaux, Piscine privée, Jeux, Calme et Nature
+Capacité :  8 personnes + BB Location : week-end, semaine, vacances</p>
+    </div>
+    <div class="mini-photos d-flex justify-content-around">
+    <img src="./PHOTOS/les moutons.jpg" alt="photo de moutons"><img src="./PHOTOS/piscine-fleur.jpg" alt="photos de piscine"><img src="./PHOTOS/panier-garni.jpg" alt="photo panier garni">
+    </div>
+
+    <!-- =================================================== CAROUSEL =================================================== -->
+
+    <div id="carouselExampleCaptions" class="carousel slide mt-5">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./PHOTOS/de37d933_original.jpg" class="d-block w-100" alt="photo terasse">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Terrasse Ouest</h5>
+        <p>Pisicne à débordement d'environ 50 m2, avec formes arrondies : première marche de la piscine destinée aux BB, et touts petits.
+Espace clos et sécurisé, alarme piscine et bâche de protection hivernale.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./PHOTOS/IMG_20190809_175931026 (1).jpg" class="d-block w-100" alt="photo terasse">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Pergola</h5>
+        <p>Espace détente ; Canapé, fauteuil, table basse,Espace repas : table et chaises 8 personnes,Cuisine d'été : frigo, évier, placards, , BBQ, PLANCHA, Chaîne HIFI Bluetooth pour les soirées musicales. Placard : Jeux d'eau, jeux enfants, ballon, bouée BB..........
+
+Douche extérieure</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./PHOTOS/IMG_20190809_180114274.jpg" class="d-block w-100" alt="photo terasse">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Piscine</h5>
+        <p>Espace bronzette :  Bains de soleil et parasols.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- ================================================= CAROUSEL 2 =============================================== -->
+<div id="carouselExampleCaptions2" class="carousel carousel2 slide mt-5">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./PHOTOS/IMG_20190809_181432701.jpg" class="d-block w-100" alt="photo terasse">
+      <div class="carousel-caption d-none d-md-block">
+          <h5 class="text-light">Terasse Sud</h5>
+          <p class="text-light">STerrasse en rez de jardin : plus fraîche, et abritée du mistral, Espace repas 10 personnes, parasol.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./PHOTOS/IMG_20190809_181453450 (1).jpg" class="d-block w-100" alt="photo jardin">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Jardin</h5>
+        <p>Grand espace arboré et fleuri, Boulodrome électrifié, ( boules enfants/adultes) PING PONG (raquette et balles) BABY FOOT (un vrai!).</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./PHOTOS/IMG_20190809_181646792 (1).jpg" class="d-block w-100" alt="photo jardin">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- ============================================ CAROUSEL 3 ========================================================= -->
+<div id="carouselExampleCaptions3" class="carousel carousel3 slide mt-5">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="./PHOTOS/28c729b4_original - Copie.jpg" class="d-block w-100" alt="photo chambre">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Chambre bleu</h5>
+        <p>1 lit 140x200, téléviseur, moustiquaire, brasseur d'air, climatisation.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./PHOTOS/La-chambreMauve.jpg" class="d-block w-100" alt="photo chambre">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Chambre Mauve</h5>
+        <p>1 lit 140 x 190, moustiquaire, brasseur d'air.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="PHOTOS/7f4dbe02_original.jpg" class="d-block w-100" alt="photo chambre">
+      <div class="carousel-caption d-none d-md-block">
+        <h5 class="text-light">Chambre Mer</h5>
+        <p class="text-light">1 lit 140 x 190, moustiquaire, brasseur d'air.</p>
+      </div>
+    </div>
+     <div class="carousel-item">
+      <img src="./PHOTOS/e70eeecd_original - Copie.jpg" class="d-block w-100" alt="photo chambre">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Chambre Grise </h5>
+        <p>moustiquaire, brasseur d'air, 2 lits 90 x 190, une chauffeuse appoint, KIT BB Complet : lit parapluie avec linge de lit, chaise haute, pot, Babyphone, couverts BB et biberon, jouets BB.....</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- ========================================================== CAROUSEL 4 ============================================= -->
+<div id="carouselExampleCaptions4" class="carousel carousel4 slide mt-5">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="PHOTOS/41BA7799-78EC-481A-872A-518BFD227473_1_105_c.jpeg" class="d-block w-100" alt="photo cuisine">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Cuisine</h5>
+        <p>Frigo, congélateur, four, miro-ondes, plaque 4 feux, grille pain,  cafatière SENSEO, bouilloire, robot, cocottes, vaisselle....</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="PHOTOS/C2BC69C0-1817-4F76-BB00-5F8BA12FFA78_1_105_c.jpeg" class="d-block w-100" alt="photo cuisine">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="./PHOTOS/d4f970b8-2285-4fef-95f0-affba404e7d0.c10 - Copie.jpg" class="d-block w-100" alt="photo salle a manger">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Salle à manger</h5>
+        <p>Grande salle à manger donnant sur terrasse piscine. Table de ferme, 8 places / chaise haute BB, vaisselier avec plusieurs services à vaisseLle, verres à pied....., billot.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+</main>
+    
+
+<?php require './php/footer.php'; ?>
---
+++ php/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap');
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="location à la semaine dans un cadre ydillique">
+    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300&display=swap" rel="stylesheet">
+    <title>Le clos de l'ermitage</title>
+    <link rel="icon" type="image/png" sizes="96x96" href="../PHOTOS/cigale-olive-removebg-preview.png">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+       <nav class="container-fluid">
+        <ul class="list-unstyled d-flex justify-content-around" >
+            <li><img src="../PHOTOS/cigale-olive-removebg-preview.png" alt="logo-cigale" style="width:80px; heigth: 80px" class="logo-navbar"></li>
+            <li class="mt-4 text-navbar">acceuil</li>
+            <li class="mt-4 text-navbar">services</li>
+            <li class="mt-4 text-navbar">contact</li>
+        </ul>
+       </nav>         
+    </header>
+</body>
